b200a6511f8f4d9234218f1d96ca8201846dc396
‘namelx’ <1536841004@qq.com>
2019-03-05 13:27:42 +0800

    version1.2

--- src/main/java/com/neusoft/Home.java
+++ src/main/java/com/neusoft/Home.java
@@ -2,6 +2,6 @@ package com.neusoft;
 
 public class Home {
     public static void main(String[] args) {
-        System.out.println("1234567");
+        System.out.println("1234567eeee");
     }
 }
